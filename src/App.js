8361e8c84fd88316e5c290e0ef951258f445baa2
duonglam306 <lamduong11201@gmail.com>
2022-03-08 14:34:13 +0700

    Create router Rating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, BrowserRouter as Router, Switch, NavLink } from "react-router-dom";
 import "./App.css";
-import HomePage from "./Address/Js/HomePage";
+import HomePage from "./HomePage";
 import Address from "./Address/Js/Address";
+import Rating from "./Rating/js/rating";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route exact path="/address" component={Address} />
+                    <Route exact path="/rating" component={Rating} />
                 </Switch>
             </Router>
         </div>
---
+++ src/Rating/js/rating.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function rating() {
+  return (
+    <div> <h1>rating</h1> </div>
+  )
+}
+
+export default rating
