c177c2e17636b97f18df27eeca3260d8e5aae8fd
maiduclong11102001 <92844614+maiduclong11102001@users.noreply.github.com>
2022-03-15 19:52:12 +0700

    Modify Header

--- src/Component/Header/Js/Header.js
+++ src/Component/Header/Js/Header.js
@@ -1,37 +1,30 @@
 import React from "react";
 import logo from "../Image/Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faShoppingCart } from "@fortawesome/fontawesome-free-solid";
+import { faBars, faShoppingCart, faUser } from "@fortawesome/fontawesome-free-solid";
+import user from "../Image/user.png";
 import "../Css/Header.css";
 
 const Header = () => {
     return (
         <div className="header sticky-top">
             <div className="container pt-1">
-                <div className="row sign_in_header justify-content-end mr-1">
+                {/* <div className="row sign_in_header justify-content-end mr-1">
                     <div className="d-flex text-light">
-                        <p className="pr-3 border-right border-light">
-                            Sign Up
-                        </p>
+                        <p className="pr-3 border-right border-light">Sign Up</p>
                         <p className="pl-3">Sign In</p>
                     </div>
-                </div>
+                </div> */}
                 <div className="row">
                     <div className="col-md-2 col-xl-3 d-none d-md-flex align-items-center">
                         <div className="col-5 col-md-12 col-xl-5">
-                            <img
-                                className="img-fluid"
-                                src={logo}
-                                alt="logoimg"
-                            />
+                            <img className="img-fluid" src={logo} alt="logoimg" />
                         </div>
 
-                        <h5 className="col-7 mb-0 d-none d-xl-block text-light">
-                            F R T N R STORE
-                        </h5>
+                        <h5 className="col-7 mb-0 d-none d-xl-block text-light">F R T N R STORE</h5>
                     </div>
 
-                    <div className="col-10 col-md-8 col-xl-7 align-self-center position-relative">
+                    <div className="col-8 col-md-8 col-xl-7 align-self-center position-relative">
                         <form className="form-inline my-1 justify-content-end">
                             <input
                                 className="form-control mr-sm-2 w-100"
@@ -48,15 +41,22 @@ const Header = () => {
                         </form>
                     </div>
 
-                    <div className="col-2 align-self-center d-flex justify-content-end">
-                        <FontAwesomeIcon className="text-light icon-cart-shopping" icon={faShoppingCart} />
+                    <div className="col-4 col-md-2 align-self-center d-flex justify-content-end">
+                        <FontAwesomeIcon
+                            className="text-light icon-cart-shopping mr-4"
+                            icon={faUser}
+                        />
+                        <FontAwesomeIcon
+                            className="text-light icon-cart-shopping"
+                            icon={faShoppingCart}
+                        />
                     </div>
                 </div>
 
-                <div className="row border-top border-light justify-content-md-start">
-                    <nav class="navbar navbar-expand-lg navbar-light p-0 m-1">
+                <div className="row border-top border-light justify-content-md-around mt-2">
+                    <nav className="navbar navbar-expand-lg navbar-light p-0 m-1">
                         <button
-                            class="navbar-toggler"
+                            className="navbar-toggler"
                             type="button"
                             data-toggle="collapse"
                             data-target="#navbarSupportedContent"
@@ -68,46 +68,40 @@ const Header = () => {
                             <FontAwesomeIcon className="text-light" icon={faBars} />
                         </button>
 
-                        <div
-                            class="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            <ul class="navbar-nav align-items-center">
-                                <li class="nav-item active">
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav align-items-center">
+                                <li className="nav-item active">
                                     <a
-                                        class="nav-link text-light ml-md-5 mr-md-5"
+                                        className="nav-link text-light ml-md-5 mr-md-5"
                                         href="/table"
                                     >
                                         TABLE
                                     </a>
                                 </li>
-                                <li class="nav-item active">
-                                    <a
-                                        class="nav-link text-light ml-md-5 mr-md-5"
-                                        href="/desk"
-                                    >
+                                <li className="nav-item active">
+                                    <a className="nav-link text-light ml-md-5 mr-md-5" href="/desk">
                                         DESK
                                     </a>
                                 </li>
-                                <li class="nav-item active">
+                                <li className="nav-item active">
                                     <a
-                                        class="nav-link text-light ml-md-5 mr-md-5"
+                                        className="nav-link text-light ml-md-5 mr-md-5"
                                         href="/chair"
                                     >
                                         CHAIR
                                     </a>
                                 </li>
-                                <li class="nav-item active">
+                                <li className="nav-item active">
                                     <a
-                                        class="nav-link text-light ml-md-5 mr-md-5"
+                                        className="nav-link text-light ml-md-5 mr-md-5"
                                         href="/bedding"
                                     >
                                         BEDDING
                                     </a>
                                 </li>
-                                <li class="nav-item active">
+                                <li className="nav-item active">
                                     <a
-                                        class="nav-link text-light ml-md-5"
+                                        className="nav-link text-light ml-md-5"
                                         href="/import-design"
                                     >
                                         CUSTOM DESIGN
