2187c0df0f8e535bd7b0da5d3a023e0be3008855
phuoc <lehuyphuoc0105@gmail.com>
2022-03-25 19:24:04 +0700

    menu

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import HomePage from "./HomePage";
 import Address from "./Component/Address/Js/Address";
 import Rating from "./Rating/js/rating";
+import Menu from "./Component/Menu/menu";
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Switch>
-                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/homepage" component={HomePage} />
                     <Route exact path="/address" component={Address} />
                     <Route exact path="/rating" component={Rating} />
+                    <Route exact path="/menu" component={Menu} />
                 </Switch>
             </Router>
         </div>
