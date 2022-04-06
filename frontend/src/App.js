b9eaa7d30d28dd4be8de731990af9fb44ebdb323
maiduclong11102001 <92844614+maiduclong11102001@users.noreply.github.com>
2022-04-06 10:32:34 +0700

    Update App.js and  index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./HomePage";
+import Address from "./Component/Address/Js/Address";
+import OrderProcess from "./Component/OrderProcess/js/OrderProcess";
+import PageProduct from "./Component/PageProduct/js/pageProduct";
+import Mapbox from "./Component/Address/Js/Mapbox";
+import Rating from "./Component/Rating/js/rating";
+import ImportDesign from "./Component/ImportDesign/js/importDesign";
+import Menu from "./Component/Menu/menu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/import-design" component={ImportDesign} />
+                    <Route exact path="/page-product" component={PageProduct} />
+                    <Route exact path="/import-design" component={ImportDesign} />
+                    <Route exact path="/rating" component={Rating} />
+                    <Route exact path="/order" component={OrderProcess} />
+                    <Route exact path="/address" component={Address} />
+                    <Route exact path="/mapbox" component={Mapbox} />
+                    <Route exact path="/homepage" component={HomePage} />
+                    <Route exact path="/address" component={Address} />
+                    <Route exact path="/menu" component={Menu} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
