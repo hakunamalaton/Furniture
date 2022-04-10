fc63501e4cda02cf631e18ec4d58430575047f95
toan lam <lamtoan557@gmail.com>
2022-04-10 15:53:20 +0700

    Fix file index.html

--- backend/app/models/order.rb
+++ backend/app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :products
+
+  after_save :do_something
+
+  private
+
+  def do_something
+    p "Yup, I have done something"
+  end
 end
