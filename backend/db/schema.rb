b7de7c08a94a41999b5432e632b8aa2758aa1a76
duonglam306 <lamduong11201@gmail.com>
2022-04-27 14:17:00 +0700

    merge toan

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_24_155945) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_062957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,7 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_24_155945) do
   end
 
   create_table "ratings", force: :cascade do |t|
-    t.string "title"
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.string "description"
