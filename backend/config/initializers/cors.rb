a6b808e60229bfe5a4022886fb40b44a32de0f0f
toan lam <lamtoan557@gmail.com>
2022-05-26 10:41:12 +0700

    Change origins in cors file

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:3000"
+      origins "https://ec-furniture-ui.herokuapp.com"
   
       resource "*",
         headers: :any,
